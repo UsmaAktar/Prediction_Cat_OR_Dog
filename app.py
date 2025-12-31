15dec472ee324c05c891e1acb981656f8f824991
Usma Aktar <usma.cse101@gmail.com>
2025-12-31 06:21:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+# -------------------------------
+# App title
+# -------------------------------
+st.title("🐱🐶 Cats vs Dogs Classifier")
+st.write("Upload an image and the model will predict whether it's a **Cat** or a **Dog**.")
